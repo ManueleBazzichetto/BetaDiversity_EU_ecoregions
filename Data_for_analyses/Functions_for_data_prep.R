f4ad8df3e99732915929423ed6a8e7653114a1ed
ManueleBazzichetto <manuele.bazzichetto@gmail.com>
2026-08-10 18:32:33 +0200

    Added code of function for creating tables for distance-decay models

--- Data_for_analyses/Functions_for_data_prep.R
+++ Data_for_analyses/Functions_for_data_prep.R
@@ -1,4 +1,6 @@
-#functions needed for preparing gdm input data
+
+
+# --------------------------------------- 1) Functions used for preparing tables formatted for GDMs
 
 #-- function to obtain all possible pairs of duplicates
 
@@ -350,4 +352,247 @@ head(test_to_del_3)
 all.equal(test_to_del$distance, test_to_del_3$distance) #TRUE
 
 rm(test_to_del, test_to_del_2, to_drop_del, test_to_del_3, test_to_del_4)
+
+
+
+# --------------------------------------- 2) Functions used for preparing tables for distance-decay models
+
+
+#first, create an example on a small dataset outside the function
+
+test_small_vegmat <- EVA_veg_forest$TyrAdr_smf$Period1
+test_small_predmat <- Matched_datasets_forest$TyrAdr_smf$Period1
+
+#PlotID is the first column; geo coords included in semi-last and last column
+colnames(test_small_vegmat)[1] #"PlotID"
+colnames(test_small_vegmat)[c((ncol(test_small_vegmat)-1), ncol(test_small_vegmat))] #"X_laea" "Y_laea"
+
+#extract PlotID, the geo coords, and plot size from test_small_predmat - these fields will be used to create a tmp predData when using formatsitepair
+test_small_predmat <- test_small_predmat[c("PlotID", "X_laea", "Y_laea", "Releve_area_m2", "PlotID_cov")]
+
+#drop coords columns from the site x species matrix
+test_small_vegmat <- test_small_vegmat[setdiff(colnames(test_small_vegmat), c("X_laea", "Y_laea"))]
+
+#compute Bray-Curtis index using formatsitepair
+#The table includes the weights column, as well as the geo coordinates for each plot pair. These columns can be excluded because
+#the weights column is only relevant for fitting gdms, while the geo distance between plot pairs will be computed using sf.
+test_small_bc <- gdm::formatsitepair(bioData = test_small_vegmat, bioFormat = 1, abundance = TRUE, siteColumn = 'PlotID',
+                                     XColumn = 'X_laea', YColumn = 'Y_laea', predData = test_small_predmat)
+
+
+#select only relevant columns
+test_small_bc <- test_small_bc[c('distance', 's1.Releve_area_m2', 's1.PlotID_cov', 's2.Releve_area_m2', 's2.PlotID_cov')]
+
+#PlotID_cov columns are pasted to create a single PlotID_pairs column
+test_small_bc$PlotID_pairs <- paste(test_small_bc$s1.PlotID_cov, test_small_bc$s2.PlotID_cov, sep = '-')
+
+#the individual PlotID columns can be excluded
+test_small_bc <- test_small_bc[c('distance', 's1.Releve_area_m2', 's2.Releve_area_m2', 'PlotID_pairs')]
+
+#compute the geographical (Euclidean) distance between sites
+
+#select only the PlotID and coords columns in test_small_predmat to create an sf object
+test_small_sp <- st_as_sf(x = test_small_predmat[c('PlotID', 'X_laea', 'Y_laea')], coords = c('X_laea', 'Y_laea'), crs = 3035)
+
+#--compute Euclidean distance between plot pairs
+test_small_dist <- st_distance(x = test_small_sp, y = test_small_sp, by_element = F, which = 'Euclidean')
+
+class(test_small_dist) #units
+
+#check if a matrix is returned when the sf object has no crs assigned (see help of st_distance())
+test_small_sp_v2 <- st_as_sf(x = test_small_predmat[c('PlotID', 'X_laea', 'Y_laea')], coords = c('X_laea', 'Y_laea'))
+
+test_small_dist_v2 <- st_distance(x = test_small_sp_v2, y = test_small_sp_v2, by_element = F, which = 'Euclidean')
+
+class(test_small_dist_v2) #Yes, a matrix array is returned (easier to work with it)
+
+#check order of plot-pairs in distance matrix - this order must match that in table formatted for gdms
+
+head(test_small_bc)
+head(test_small_sp_v2)
+head(test_small_dist_v2[1:10, 1:10])
+
+#the lower triangle of the distance matrix is returned
+st_distance(x = test_small_sp_v2[1, ], y = test_small_sp_v2[2, ], which = 'Euclidean') #403597-403706
+st_distance(x = test_small_sp_v2[1, ], y = test_small_sp_v2[3, ], which = 'Euclidean') #403597-403707
+st_distance(x = test_small_sp_v2[1, ], y = test_small_sp_v2[4, ], which = 'Euclidean') #403597-403710
+st_distance(x = test_small_sp_v2[1, ], y = test_small_sp_v2[5, ], which = 'Euclidean') #403597-403711
+
+head(test_small_bc[1237:1239, ])
+
+st_distance(x = test_small_sp_v2[2, ], y = test_small_sp_v2[3, ], which = 'Euclidean') #403706-403707
+
+#The lower.tri of the matrix matches the order produced by formatsitepair
+class(test_small_dist_v2[lower.tri(test_small_dist_v2)])
+
+#test equality between distance matrices returned by st_distance when setting (or non setting) crs in st_as_sf
+all.equal(as.numeric(test_small_dist[lower.tri(test_small_dist)]), test_small_dist_v2[lower.tri(test_small_dist_v2)]) #T
+
+#--use decostand to transform abundance matrix into 1/0 matrix
+
+#check class of obj returned by decostand
+class(decostand(x = test_small_vegmat[-1], method = 'pa')) #data.frame
+
+#give it a look
+#decostand(x = test_small_vegmat[-1], method = 'pa')[1:10, 1:10]
+
+#create p/a version of test_small_vegmat
+test_small_vegmat_pa <- cbind(test_small_vegmat[1], decostand(test_small_vegmat[-1], method = 'pa'))
+
+identical(test_small_vegmat_pa[1], test_small_vegmat[1]) #T
+
+#compute Jaccard and Horn-Morisita and create final output
+
+#horn
+test_small_horn <- gdm::formatsitepair(bioData = test_small_vegmat, bioFormat = 1, abundance = TRUE, siteColumn = 'PlotID',
+                                       XColumn = 'X_laea', YColumn = 'Y_laea', predData = test_small_predmat, dist = 'horn')
+
+#jaccard
+test_small_jac <- gdm::formatsitepair(bioData = test_small_vegmat_pa, bioFormat = 1, abundance = FALSE, siteColumn = 'PlotID',
+                                      XColumn = 'X_laea', YColumn = 'Y_laea', predData = test_small_predmat, dist = 'jaccard')
+
+
+#check the order of PlotID is maintained across datasets
+identical(test_small_horn[c('s1.PlotID_cov', 's2.PlotID_cov')], test_small_jac[c('s1.PlotID_cov', 's2.PlotID_cov')]) #T
+
+#create final output
+test_final_out <- cbind(test_small_bc, horn = test_small_horn[[1]], jaccard = test_small_jac[[1]],
+                        euc_dist = test_small_dist_v2[lower.tri(test_small_dist_v2)])
+
+colnames(test_final_out)[1] <- 'bray'
+
+#create a column including difference in plot size
+test_final_out$Abs_diff_plot_size <- with(test_final_out, abs(s1.Releve_area_m2 - s2.Releve_area_m2))
+
+#drop columns with individual plots' size
+test_final_out$s1.Releve_area_m2 <- NULL
+test_final_out$s2.Releve_area_m2 <- NULL
+
+#re-order columns
+test_final_out <- test_final_out[c(1, 3, 4, 5, 2, 6)]
+
+hist(test_final_out$Abs_diff_plot_size)
+hist(sqrt(test_final_out$Abs_diff_plot_size))
+plot(test_final_out$Abs_diff_plot_size, test_final_out$bray)
+
+
+#function for computing several dissimilarity indices and geographic distance among plot pairs
+
+
+get_distances_ind <- function(veg_mat, pred_mat) {
+  
+  #load required packages
+  require(gdm)
+  require(vegan)
+  require(sf)
+  
+  #check that PlotID, X_laea, Y_laea, and Releve_area_m2 are included in both veg_mat and pred_mat
+  check_cols <- c("PlotID", "X_laea", "Y_laea", "Releve_area_m2", "PlotID_cov")
+  
+  if(!isTRUE(all(check_cols[c(1, 2, 3)] %in% colnames(veg_mat)) & all(check_cols %in% colnames(pred_mat)))) stop('veg_mat must include PlotID, coords, and pred_mat also plot size and PlotID_cov')
+  
+  #select only check_cols in pred_mat
+  pred_mat <- pred_mat[check_cols]
+  
+  #create spatial object for computing Euclidean distance between plot pairs
+  predmat_sp <- st_as_sf(x = pred_mat[c("PlotID", "X_laea", "Y_laea")], coords = c("X_laea", "Y_laea"))
+  
+  #compute Euclidean distance matrix and return its upper triangle
+  euc_dist <- st_distance(x = predmat_sp, y = predmat_sp, by_element = F, which = 'Euclidean')
+  
+  #extract lower triangle
+  euc_dist <- euc_dist[lower.tri(euc_dist)]
+  
+  #rm predmat_sp
+  rm(predmat_sp)
+  
+  #drop coordinates columns from veg_mat
+  veg_mat <- veg_mat[setdiff(colnames(veg_mat), c("X_laea", "Y_laea"))]
+  
+  #compute indices
+  res <- do.call(cbind, lapply(c('bray', 'horn', 'jaccard'), function(ind) {
+    
+    if(ind == 'jaccard') {
+      
+      #extract PlotID col and drop it from veg_mat
+      plot_id_col <- veg_mat['PlotID']
+      
+      #drop PlotID col from veg_mat
+      veg_mat <- veg_mat[setdiff(colnames(veg_mat), 'PlotID')]
+      
+      #use decostand to transform abund data to p/a
+      veg_mat <- vegan::decostand(x = veg_mat, method = 'pa')
+      
+      #cbind plot_id_col
+      veg_mat <- cbind(plot_id_col, veg_mat)
+      
+      #compute jaccard
+      jac_mat <- gdm::formatsitepair(bioData = veg_mat, bioFormat = 1, abundance = FALSE, siteColumn = 'PlotID',
+                                     XColumn = 'X_laea', YColumn = 'Y_laea', predData = pred_mat, dist = ind)
+      
+      #return res - only distance column, i.e. the value of the Jaccard index
+      jac_mat <- jac_mat[c('distance')]
+      
+      #rename col
+      colnames(jac_mat) <- ind
+      
+      return(jac_mat)
+      
+      } else {
+        
+        #compute ind
+        ind_mat <- gdm::formatsitepair(bioData = veg_mat, bioFormat = 1, abundance = TRUE, siteColumn = 'PlotID',
+                                     XColumn = 'X_laea', YColumn = 'Y_laea', predData = pred_mat, dist = ind)
+        
+        if(ind == 'horn') {
+          
+          #extract column including values of horn index
+          ind_mat <- ind_mat[c('distance')]
+          
+          colnames(ind_mat) <- ind
+          
+          return(ind_mat)
+          
+          } else {
+            
+            #extract column including values of bray index
+            colnames(ind_mat)[1] <- ind
+            
+            ind_mat <- ind_mat[c(ind, 's1.Releve_area_m2', 's1.PlotID_cov', 's2.Releve_area_m2', 's2.PlotID_cov')]
+            
+            return(ind_mat)
+            
+          }
+        
+        }
+    
+    }))
+  
+  #create col with plot id pairs and drop individual plot id columns
+  res$PlotID_pairs <- paste(res[['s1.PlotID_cov']], res[['s2.PlotID_cov']], sep = '-')
+  
+  #create col with abs difference in plot size and drop individual plot size columns
+  res$Abs_diff_plot_size <- abs(res[['s1.Releve_area_m2']] - res[['s2.Releve_area_m2']])
+  
+  #also attach geo dist col
+  res$euc_dist <- euc_dist
+  
+  #only keep id and plot pairs cols
+  res <- res[c('bray', 'horn', 'jaccard', 'euc_dist', 'PlotID_pairs', 'Abs_diff_plot_size')]
+  
+  #return result
+  return(res)
+  
+  }
+
+
+identical(x = test_final_out, y = get_distances_ind(veg_mat = EVA_veg_forest$TyrAdr_smf$Period1, pred_mat = Matched_datasets_forest$TyrAdr_smf$Period1)) #TRUE
+
+
+rm(test_small_vegmat, test_small_predmat, test_small_bc, test_small_sp, test_small_dist, test_small_sp_v2,
+   test_small_dist_v2, test_small_vegmat_pa, test_small_horn, test_small_jac, test_final_out)
+
+
+
+
 
